afe4fc8c270b46cdda60d3bd03e3927c30ec749b
“Aslanbek <aslanbekasylbekov@MacBook-Air-Aslanbek-2.local>
2025-11-25 12:43:13 +0500

    upd

--- assignment 2/resources/testData.py
+++ assignment 2/resources/testData.py
@@ -1,4 +1,9 @@
 base_url = 'https://www.demoblaze.com/'
+BROWSERSTACK_USERNAME = "bsuser_0KWvM8"
+BROWSERSTACK_ACCESS_KEY = "wdcP9mSHhVt1gdR13wzS"
+
+BROWSERSTACK_REMOTE_URL = f"http://{BROWSERSTACK_USERNAME}:{BROWSERSTACK_ACCESS_KEY}@hub.browserstack.com/wd/hub"
+BROWSER = 'chrome'
 
 user_login_value = 'aslanbek@test.gmail.com'
 user_password_value = '123456789'
